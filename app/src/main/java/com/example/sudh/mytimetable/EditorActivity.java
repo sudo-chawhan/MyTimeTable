5071034419f577d282dd5358954a580ef51daa88
sudo-chawhan <cs16btech11037@iith.ac.in>
2017-12-26 19:47:19 +0530

    adding editorActivity

---
+++ app/src/main/java/com/example/sudh/mytimetable/EditorActivity.java
@@ -0,0 +1,18 @@
+package com.example.sudh.mytimetable;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sudo-chawhan on 26/12/17.
+ */
+
+public class EditorActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_editor);
+    }
+}
--- app/src/main/java/com/example/sudh/mytimetable/MainActivity.java
+++ app/src/main/java/com/example/sudh/mytimetable/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sudh.mytimetable;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setting layout references
+        RelativeLayout slot_one = (RelativeLayout) findViewById(R.id.slot_one);
+        RelativeLayout slot_two = (RelativeLayout) findViewById(R.id.slot_two);
+        RelativeLayout slot_three = (RelativeLayout) findViewById(R.id.slot_three);
+        RelativeLayout slot_four = (RelativeLayout) findViewById(R.id.slot_four);
+        RelativeLayout slot_five = (RelativeLayout) findViewById(R.id.slot_five);
+        RelativeLayout slot_six = (RelativeLayout) findViewById(R.id.slot_six);
+
+        //setting on clock listeners for each slot
+        slot_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        slot_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        slot_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        slot_four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        slot_five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        slot_six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
